93dfbf5f6cdc7b4a064849fa48b24b345f54615d
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-03-08 16:45:31 +0800

    UI UX Weather Card

--- src/views/pages/Weather/Weather.vue
+++ src/views/pages/Weather/Weather.vue
@@ -1,15 +1,32 @@
 <template>
-  <ion-page id="main-content">
-    <ion-card>
-      <ion-card-header>
-        <ion-card-title>Card Title</ion-card-title>
-        <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-      </ion-card-header>
+  <ion-page>
+    <ion-content>
+      <ion-card class="weather-card">
+        <ion-card-header>
+          <ion-card-subtitle class="text-white" style="font-size: 17px"
+            >Lipa City, Batangas Philippines</ion-card-subtitle
+          >
+          <ion-row style="margin-top: 15px">
+            <ion-col class="ion-text-start">
+              <ion-card-title class="text-white" style="font-size: 40px"
+                >30°C</ion-card-title
+              >
+            </ion-col>
+            <ion-col class="ion-text-center" v-if="weather">
+              <ion-img
+                :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"
+                alt="weather-icon"
+              ></ion-img>
+              <!-- {{ weather.weather.icon }} -->
+            </ion-col>
+          </ion-row>
+        </ion-card-header>
 
-      <ion-card-content>
-        <pre>{{ weatherData }}</pre>
-      </ion-card-content>
-    </ion-card>
+        <ion-card-content>
+          <!-- <pre>{{ weather }}</pre> -->
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
   </ion-page>
 </template>
 
@@ -28,16 +45,23 @@ import {
   IonTabButton,
   IonLabel,
   IonIcon,
+  IonCol,
+  IonGrid,
+  IonRow,
+  IonImg,
 } from "@ionic/vue";
 import { onMounted, ref } from "vue";
 import api from "@/api";
+import axios from "axios";
 
-const weatherData = ref([]);
+const weather = ref(null);
 const fetchData = async () => {
-  const response = await api.get(
-    "/weather?lat=13.940776257881716&lon=121.16507619074791&appid=2de3397f7de9039ea1c0523d3c605269"
+  const response = await axios.get(
+    `https://api.weatherstack.com/current?access_key=${
+      import.meta.env.VITE_API_KEY_SECONDARY
+    }&query=Lipa`
   );
-  weatherData.value = response.data;
+  weather.value = response.data;
 };
 
 onMounted(async () => {
@@ -45,4 +69,18 @@ onMounted(async () => {
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.weather-card {
+  background: radial-gradient(
+    ellipse at 67% 82%,
+    #fa4d5e,
+    rgba(250, 47, 179, 1)
+  );
+  border-radius: 15px !important;
+  padding: 10px;
+}
+
+.text-white {
+  color: #fff !important;
+}
+</style>
