b682aa38d7c38b9f44e65a4561ceccbeb70b8143
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-03-08 14:04:32 +0800

    UI UX for navigation tabs

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,59 +1,78 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>E-Weather</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="home" href="/home">
+          <ion-icon :icon="cloudy" />
+          <ion-label>Weather</ion-label>
+        </ion-tab-button>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+        <ion-tab-button tab="radio" href="/radio">
+          <ion-icon :icon="call" />
+          <ion-label>Emergency</ion-label>
+        </ion-tab-button>
 
-      <div id="container">
+        <ion-tab-button tab="library" href="/library">
+          <ion-icon :icon="warning" />
+          <ion-label>Guidelines</ion-label>
+        </ion-tab-button>
 
-        <ion-card>
-          <ion-card-header>
-            <ion-card-title>Card Title</ion-card-title>
-            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-          </ion-card-header>
-
-          <ion-card-content>
-           <pre>{{ weatherData }}</pre>
-          </ion-card-content>
-        </ion-card>
-
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+        <ion-tab-button tab="search" href="/search">
+          <ion-icon :icon="person" />
+          <ion-label>Profile</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
   </ion-page>
 </template>
 
 <script setup>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import {onMounted, ref} from 'vue'
-import api from "@/api"
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonLabel,
+  IonIcon,
+} from "@ionic/vue";
+import {
+  playCircle,
+  radio,
+  library,
+  search,
+  cloudy,
+  person,
+  call,
+  warning,
+} from "ionicons/icons";
+import { onMounted, ref } from "vue";
+import api from "@/api";
 
-const weatherData = ref([])
+const weatherData = ref([]);
 const fetchData = async () => {
-    const response = await api.get('/weather?lat=13.940776257881716&lon=121.16507619074791&appid=2de3397f7de9039ea1c0523d3c605269')
-    weatherData.value = response.data
-}
+  const response = await api.get(
+    "/weather?lat=13.940776257881716&lon=121.16507619074791&appid=2de3397f7de9039ea1c0523d3c605269"
+  );
+  weatherData.value = response.data;
+};
 
 onMounted(async () => {
-  await fetchData()
-})
-
+  await fetchData();
+});
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -69,9 +88,9 @@ onMounted(async () => {
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
