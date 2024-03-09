47d2bb9bee7ceb0fc0c8b3027ce7fb6ce3c51e47
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-03-09 15:24:10 +0800

    Card UI UX

--- src/views/pages/Weather/Weather.vue
+++ src/views/pages/Weather/Weather.vue
@@ -1,34 +1,39 @@
 <template>
   <ion-page>
     <ion-content>
-      <div class="grid mb-0" style="border: 1px solid red;">
-        <div class="col-5">
-          <div class="text-start text-xl px-3 pt-3 border-round-sm font-semibold" style="color: #4F4F4F;">
-            <!-- <ion-icon :icon="navigate" /> -->
-            Lipa City, Batangas
-            Philippines
+      <div class="grid mb-0">
+        <div class="col-6">
+          <div class="text-end text-xl px-3 pt-3 border-round-sm font-semibold" style="color: #4F4F4F;">
+            AppName <ion-icon :icon="cloudyNight" />
           </div>
         </div>
-        <div class="col-7">
-          <!-- <div class="text-end p-3 border-round-sm bg-primary font-bold ">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d123911.53933867977!2d121.01227609966392!3d13.9445724775512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd13576170055f%3A0xe6da25b39082662a!2sLipa%2C%20Batangas!5e0!3m2!1sfil!2sph!4v1709954443373!5m2!1sfil!2sph"
-              width="100" height="60" style="border:0;" allowfullscreen="" loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </div> -->
+        <div class="col-6">
+          <div class="text-right px-3 pt-3 border-round-sm bg-primary font-bold">
+            <button class="btn-sign">Sign In</button>
+          </div>
         </div>
       </div>
       <ion-card class="weather-card" v-if="weatherDatav2">
+        <ion-card-header class="p-0 m-0">
+          <div class="flex">
+            <div class="col-12">
+              <div class="text-left p-0">
+                <ion-card-title class="text-white">Lipa City <span style="font-size: 15px;">Batangas
+                    Philippines</span></ion-card-title>
+              </div>
+            </div>
+          </div>
+        </ion-card-header>
         <ion-card-content>
           <div class="grid">
             <div class="col-6 relative" v-if="weather">
               <div class=" pt-0">
                 <ion-img class="weather-icon" v-if="weatherDatav2"
                   :src="`/weather_icons/set01/big/${weatherDatav2.current.icon_num}.png`" alt="weather-icon"></ion-img>
-                <ion-label class="text-3xl text-white font-bold" style="color: #FBE1E2;">
+                <ion-label class="text-2xl text-white font-bold" style="color: #FBE1E2;">
                   {{ weatherDatav2.current.summary }}
                 </ion-label>
-                <ion-label class="text-2xl text-white" style="color: #FBE1E2;">
+                <ion-label class="text-xl text-white" style="color: #FBE1E2;">
                   Today
                 </ion-label>
               </div>
@@ -36,8 +41,8 @@
             <div class="col-6" v-if="weatherDatav2">
               <div class=" flex flex-column justify-content-end">
                 <ion-label class="text-6xl text-white font-bold" style="color: #FBE1E2;">{{
-        weatherDatav2.current.temperature
-      }}°C</ion-label>
+              weatherDatav2.current.temperature
+            }}°C</ion-label>
                 <div>
                 </div>
                 <ion-label class="text-l text-white font-bold" style="color: #FBE1E2;">
@@ -89,7 +94,7 @@ import {
   IonRow,
   IonImg,
 } from "@ionic/vue";
-import { navigate } from "ionicons/icons"
+import { navigate, logIn, cloudyNight } from "ionicons/icons"
 import { onMounted, ref, computed } from "vue";
 import api from "@/api";
 import axios from 'axios';
@@ -125,6 +130,23 @@ onMounted(async () => {
 .text-white {
   color: #fff !important;
 }
+
+.btn-sign {
+  background: #5988F9;
+  color: #fff;
+  text-align: center;
+  padding: 8px 12px;
+  border-radius: 20px;
+  font-size: 14px;
+  display: inline-block;
+  text-decoration: none;
+  font-weight: 600;
+}
+
+.btn-sign:hover {
+  background: #4c78d9;
+}
+
 
 /* For small screens */
 .weather-icon {
